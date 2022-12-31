b6a939fc249af245186d39904abc75086aee1103
anthonyredamonti <39130075+anthonyredamonti@users.noreply.github.com>
2022-12-31 10:52:20 -0500

    Add files via upload

---
+++ Principles of Operating Systems/Lab1/c_h_files/main_A40B20.c
@@ -0,0 +1,28 @@
+/* main.c - main*/
+#include <xinu.h>
+
+void sndA(void), sndB(void);
+
+/* main - Example of creating processes in Xinu */
+void main(void)
+{
+resume(create(sndA, 1024, 40, "p 1", 0));
+resume(create(sndB, 1024, 20, "p 2", 0));
+}
+
+/* sndA - Repeatedly emit 'A' on console */
+
+void sndA(void)
+{
+while(1){
+putc(CONSOLE, 'A');
+}
+}
+/* sndB - Repeatedly emit 'B' on console */
+
+void sndB(void)
+{
+while(1){
+putc(CONSOLE, 'B');
+}
+}
---
+++ Principles of Operating Systems/Lab1/c_h_files/main_helloworld.c
@@ -0,0 +1,7 @@
+/* hello.c - main*/
+#include <xinu.h>
+/* main - just say hello, then exit */
+void main(void)
+{
+printf("Hello, world!\n");
+}
