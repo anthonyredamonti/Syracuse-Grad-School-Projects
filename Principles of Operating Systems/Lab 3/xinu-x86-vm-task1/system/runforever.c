4f4829a8b49772a0b367cb73eab7b9f64a5feead
anthonyredamonti <39130075+anthonyredamonti@users.noreply.github.com>
2023-01-03 15:14:46 -0500

    Add files via upload

---
+++ Principles of Operating Systems/Lab 3/xinu-x86-vm-task1/system/runforever.c
@@ -0,0 +1,12 @@
+/* runforever.c - runforever */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  runforever  -  run forever in an infinite while-loop
+ *------------------------------------------------------------------------
+ */
+void runforever(void)
+{
+	while(1);
+}
